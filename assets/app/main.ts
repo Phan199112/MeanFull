4a527e30a132e883c08f3e453ba459337b3ea644
abruyneel <arne.bruyneel@gmail.com>
2017-12-05 22:08:31 -0800

    update

--- assets/app/main.ts
+++ assets/app/main.ts
@@ -1,7 +1,8 @@
 import './polyfills';
-
+import {enableProdMode} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from "./app.module";
 
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
