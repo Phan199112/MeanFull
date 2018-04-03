ae85528b8191b2e0891e56edd07c6a9934630946
Fernando Funes <ffunes59@ucla.edu>
2018-04-02 18:36:46 -0700

    Email fixes 2

--- functions/email.js
+++ functions/email.js
@@ -98,7 +98,6 @@ exports.sendNotificationFormRequest = function sendNotificationFormRequest(email
 
 
 exports.sendNotificationCommRequest = function sendNotificationCommRequest(email, sender, commtitle, commpic) {
-    var name = getUserDisplayName(user);
     var subject = "You have been invited to join a community on Questionsly";
     var messagesafe = "Hello! "+ name +" invited you to join a community on Questionsly. Please review the notifications page to review your pending requests. https://www.questionsly.com/settings;page=notifications";
     var dateString = new Date();
@@ -118,7 +117,6 @@ exports.sendNotificationCommRequest = function sendNotificationCommRequest(email
 
 
 exports.sendNotificationDiscussion = function sendNotificationDiscussion(email, commenter, link) {
-    var name = getUserDisplayName(user);        
     var subject = "Survey comments on Questionsly";
     var messagesafe = "Hello! " + name + " commented on your form on Questionsly. Please review the notifications page to review your pending requests. https://www.questionsly.com/settings;page=notifications";
     var dateString = new Date();
