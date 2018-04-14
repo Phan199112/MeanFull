a5ace73c862eb8601954c3f17fbd384cab7c7b7d
ncantrell <38082643+ncantrell@users.noreply.github.com>
2018-04-13 21:26:45 -0700

    Update users.login.js
    Add Google OpenID?

--- routes/users.login.js
+++ routes/users.login.js
@@ -35,7 +35,31 @@ module.exports = function(app, passport, manager, hashids) {
             // providers.
             return cb(null, profile);
         }));
-
+    
+    // =========================================================================
+    // Google OpenID SIGNIN ====================================================
+    // =========================================================================
+    // Use the GoogleStrategy within Passport.
+    //   Strategies in passport require a `validate` function, which accept
+    //   credentials (in this case, an OpenID identifier and profile), and invoke a
+    //   callback with a user object.
+    passport.use(new GoogleStrategy({
+        returnURL: 'http://localhost:3000/auth/google/return',
+        realm: 'http://localhost:3000/'
+      },
+    function(identifier, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+      
+          // To keep the example simple, the user's Google profile is returned to
+          // represent the logged-in user.  In a typical application, you would want
+          // to associate the Google account with a user record in your database,
+          // and return that user instead.
+          profile.identifier = identifier;
+          return done(null, profile);
+        });
+      }
+    ));
 
 
     // =========================================================================
