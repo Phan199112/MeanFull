08bd3af1c43eb8db3a9362d0eb522917f3b970df
Kristopher Windsor <kristopher@pinger.com>
2018-05-31 23:28:41 -0700

    Remove unused components

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -27,7 +27,6 @@ import { Angulartics2Mixpanel } from 'angulartics2/mixpanel';
 
 import {CreateFormComponent} from "./createForm/createForm.component";
 import {ShowFormComponent} from "./showForm/showForm.component";
-import {PreviewFormComponent} from "./previewForm/previewForm.component";
 import {ShareFormComponent} from "./shareForm/shareForm.component";
 import {TakeFormComponent} from "./takeForm/takeForm.component";
 import {FocusDirective} from "./focus.directive";
@@ -39,8 +38,6 @@ import {MiniShowFormComponent} from "./feed/miniShowForm.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {TagListComponent} from "./tagsContainer/tag.list.component";
 import {TagComponent} from "./tagsContainer/tag.component";
-import {CommunityComponent} from "./communityContainer/community.component";
-import {CommunityListComponent} from "./communityContainer/community.list.component";
 import {SettingsComponent} from './settings/settings.component';
 import {SettingsQuestionComponent} from './settings/question.component';
 import {ToolTipModule} from 'angular2-tooltip';
@@ -48,24 +45,17 @@ import {CreateCommunityComponent} from './createCommunity/createCommunity.compon
 import {ViewCommunityComponent} from "./viewCommunity/viewCommunity.component";
 import {FeedPageComponent } from "./feed/feed.page.component";
 import {ShortAnswers} from "./feed/shortAnswers.component";
-import {YourFormComponent } from "./yourForms/yourform.component";
-import {YourFormListComponent } from "./yourForms/yourform.list.component";
 import {FeedbackComponent } from "./feedback/feedback.component";
 import {NguiAutoCompleteModule } from '@ngui/auto-complete';
 import {TagBannerListComponent} from "./tagsBanner/tagbanner.list.component";
 import {DiscussionComponent} from "./discussion/discussion.component";
 import {DiscussionListComponent} from "./discussion/discussion.list.component";
-import {ChartJSComponent} from "./charts/chartjs.component";
 import { PieChart } from "./charts/pieChart"
 import {ChartsModule} from 'ng2-charts';
 import {NetworkComponent} from "./networkContainer/network.component";
 import {NetworkListComponent} from "./networkContainer/network.list.component";
 import {YourCommunityListComponent} from './yourCommunities/yourcomm.list.component';
 import {YourCommunityComponent} from './yourCommunities/yourcomm.component';
-import {YourFriendsListComponent} from './yourFriends/yourfriends.list.component';
-import {YourFriendsComponent} from './yourFriends/yourfriends.component';
-import {ResultsTableComponent} from "./resultstable/resultstable.component";
-import {MiniResultsTableComponent} from "./resultstable/miniresultstable.component";
 import {StarsQuestionComponent} from "./questions/stars/starsQuestion.component";
 import {ImageEditorComponent} from "./imageEditor/imageEditor.component";
 import {ResourceListComponent} from './resourceList/resourceList.component';
@@ -96,7 +86,6 @@ import { ShareService } from "./share.service";
         AppComponent,
         CreateFormComponent,
         ShowFormComponent,
-        PreviewFormComponent,
         ShareFormComponent,
         TakeFormComponent,
         FocusDirective,
@@ -105,7 +94,6 @@ import { ShareService } from "./share.service";
         FeedListComponent,
         LoginComponent,
         MiniShowFormComponent,
-        ChartJSComponent,
         ProfileComponent,
         TagListComponent,
         TagComponent,
@@ -114,10 +102,6 @@ import { ShareService } from "./share.service";
         CreateCommunityComponent,
         ViewCommunityComponent,
         FeedPageComponent,
-        CommunityComponent,
-        CommunityListComponent,
-        YourFormComponent,
-        YourFormListComponent,
         FeedbackComponent,
         NetworkComponent,
         NetworkListComponent,
@@ -126,10 +110,6 @@ import { ShareService } from "./share.service";
         DiscussionListComponent,
         YourCommunityListComponent,
         YourCommunityComponent,
-        YourFriendsListComponent,
-        YourFriendsComponent,
-        ResultsTableComponent,
-        MiniResultsTableComponent,
         Autosize,
         StarsQuestionComponent,
         ImageEditorComponent,
