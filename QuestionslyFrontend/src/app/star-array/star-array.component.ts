6fa3caeeadd8a6ed3e380090bc8b0cd4c88bd389
Kristopher Windsor <kristopher@pinger.com>
2018-06-26 00:07:54 -0700

    ng6 Add star-array.component

--- QuestionslyFrontend/src/app/star-array/star-array.component.ts
+++ QuestionslyFrontend/src/app/star-array/star-array.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, Validators} from '@angular/forms';
+
 
 @Component({
-  selector: 'app-star-array',
-  templateUrl: './star-array.component.html',
-  styleUrls: ['./star-array.component.scss']
+    selector: 'app-star-array',
+    templateUrl: './star-array.component.html',
+    styleUrls: ['./star-array.component.scss'],
+    providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: StarArrayComponent }]
 })
 export class StarArrayComponent implements OnInit {
+    @Input() rating = -1;
+    @Input() scale: any;
+    @Output() selected: EventEmitter<number> = new EventEmitter<number> ();
+    temp: Array<any> = Array(this.scale);
+    _onChange: (value: any) => void;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.scale = Number(this.scale);
+        this.temp = Array(this.scale);
+    }
 
+    writeValue(value: any) {}
+
+    registerOnChange(fn: (value:any) => void) {
+        this._onChange = fn;
+    }
+
+    registerOnTouched() {}
+
+    toggle(rating: number) {
+        window.console.log("Rating is: ", rating);
+        this.rating = rating;
+        this.selected.emit(this.rating + 1);
+    }
 }
