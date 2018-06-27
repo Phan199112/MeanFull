c09abd170ed27645433df3035e9d188808bb78e2
Kristopher Windsor <kristopher@pinger.com>
2018-06-26 23:56:02 -0700

    ng6 Add share-form.component

--- QuestionslyFrontend/src/app/share-form/share-form.component.ts
+++ QuestionslyFrontend/src/app/share-form/share-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {FormService} from '../form.service';
+import {UserService} from '../user.service';
 
 @Component({
-  selector: 'app-share-form',
-  templateUrl: './share-form.component.html',
-  styleUrls: ['./share-form.component.scss']
+    selector: 'app-share-form',
+    templateUrl: './share-form.component.html',
+    styleUrls: ['./share-form.component.scss'],
+    providers: [FormService, UserService]
 })
+
 export class ShareFormComponent implements OnInit {
+    link: string;
+    reject = false;
 
-  constructor() { }
+    constructor(private formService: FormService,
+                private userService: UserService) {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.userService.afterLoginCheck().then(userData => {
+            if (userData) {
+                this.link = `https://www.questionsly.com/takeForm/${this.formService.getData().id}`;
+            } else {
+                this.reject = true;
+            }
+        });
+    }
 }
