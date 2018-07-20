408dc11e83dd26ca5ac5fbeca15db0d64b4d61c3
Kristopher Windsor <kristopher@pinger.com>
2018-07-20 13:56:36 -0700

    Add more info for debugging the signup bug

--- QuestionslyFrontend/src/app/sign-in/sign-in.component.ts
+++ QuestionslyFrontend/src/app/sign-in/sign-in.component.ts
@@ -148,7 +148,7 @@ export class SignInComponent implements OnInit {
                 .post('/users/signup', signupData).toPromise()
                 .then(response => {
                     const responseJson = response.json();
-                    console.log(responseJson, response);
+                    console.log(responseJson, response, responseJson.status === 1, responseJson.status == 1);
                     if (responseJson.status === 1) {
                         this.http.post('/users/login/local', {email: signupData.email, password: signupData.password})
                             .map((res: any) => res.json())
